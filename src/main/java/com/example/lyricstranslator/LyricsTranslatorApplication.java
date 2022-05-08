b7a2bf593300a32fc2d0ad30d3f7a4daf4ae765c
Kajetan Wiacek <kajetanw49@gmail.com>
2022-05-08 23:54:13 +0200

    Initial commit

---
+++ src/main/java/com/example/lyricstranslator/LyricsTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lyricstranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricsTranslatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyricsTranslatorApplication.class, args);
+    }
+
+}
